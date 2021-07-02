8bc61cc299618e07333612f364416e44d6db23b2
Amala <amalahema@gmail.com>
2021-07-02 17:35:04 -0500

    HTTPS secure connection (private key)

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ connect.then((db) => {
 
 
 var app = express();
-
+//Secure Traffic Only
+app.all('*',(req,res,next) => 
+{
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);//req.url is the rest of the path name
+  }
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
